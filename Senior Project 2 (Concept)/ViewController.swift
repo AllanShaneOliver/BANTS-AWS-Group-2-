2bd68a0e722137cc2a8a8bdfc66521540c10fa58
AllanShaneOliver <allanshane.oliver@bison.howard.edu>
2023-02-01 17:30:02 -0500

    Initial Commit

---
+++ Senior Project 2 (Concept)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Senior Project 2 (Concept)
+//
+//  Created by Shane Oliver on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
